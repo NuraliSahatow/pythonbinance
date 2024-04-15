c96898a22fa0a4e79857ed4c75c725247f53fa0e
Nurali Sahatow <61908851+NuraliSahatow@users.noreply.github.com>
2024-04-15 12:58:23 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from binanceApi import *
-from coinexApi import  *
-# Вызов функции login_to_binance() для входа в аккаунт на Binance
-
-marketc = CoinEx.get_market_depth()
-#marketb = Binance.get_market_depth()
-print(marketc)
-# Вывод результата
